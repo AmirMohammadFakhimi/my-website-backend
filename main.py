c7173ea5a0e1cd28e6bb22e3260606538b20f29c
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2023-09-17 17:17:23 +0330

    change cors options

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import configparser
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
 config = configparser.ConfigParser()
 config.read('ConfigFile.ini')
 
